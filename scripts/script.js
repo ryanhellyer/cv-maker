6e355a9f33ef6e58358a97e60dd5828d0e738e38
Ryan Hellyer <ryanhellyer@gmail.com>
2024-07-01 13:12:31 +0200

    Automated Git commit

--- scripts/script.js
+++ scripts/script.js
@@ -14,26 +14,29 @@ const loadCV = async () => {
 		let sections = Array.from(main.querySelectorAll('section'));
 
 sections = sections.slice(0, 1); // JUST TESTING WITH FIRST SECTION RIGHT NOW.
-		sections.forEach(section => {
-			(async () => {
-				main.innerHTML += section.outerHTML;
 
+		for (let section of sections) {
+			main.innerHTML += section.outerHTML;
+
+			const mustacheRendered = Mustache.render(section.outerHTML, cv);
+			const rendered = unescapeHTMLElements(mustacheRendered);
+			main.innerHTML = rendered;
+
+			let i = cv.jobs.length;
+			while (i !== 0 && hasOverflowed(page)) {
+				i--;
+				cv.jobs = cv.jobs.slice(0, i); // Adjust cv data
 				const mustacheRendered = Mustache.render(section.outerHTML, cv);
 				const rendered = unescapeHTMLElements(mustacheRendered);
 				main.innerHTML = rendered;
 
-				let i = cv.jobs.length;
-				while (i !== 0 && hasOverflowed(page)) {
-					i--;
-					cv.jobs = cv.jobs.slice(0, i); // slice from index 0 to 1 (exclusive)
-					const mustacheRendered = Mustache.render(section.outerHTML, cv);
-					const rendered = unescapeHTMLElements(mustacheRendered);
-					main.innerHTML = rendered;
+				await new Promise(requestAnimationFrame);
+			}
+		}
 
-					await new Promise(requestAnimationFrame);
-				}
-			})();
-		});
+		const mustacheRendered = Mustache.render(page.innerHTML, cv);
+		const rendered = unescapeHTMLElements(mustacheRendered);
+		page.innerHTML = rendered;
 	} catch (error) {
 		console.error('Error:', error);
 	}
